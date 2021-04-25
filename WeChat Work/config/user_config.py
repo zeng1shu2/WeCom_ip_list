42addceff0e338c68c01a99b5d00f68b69d9c519
zeng1shu2 <340013129@qq.com>
2021-04-25 16:41:46 +0800

    1.0.6

--- WeChat Work/config/user_config.py
+++ WeChat Work/config/user_config.py
@@ -1,13 +1,13 @@
 # coding:utf-8
 
 mali_config = {
-    'address': '192.168.0.10',
-    'username': 'system@zlg.cn',
-    'password': 'emxnbWN1IUAjMQ==',
-    'sender': 'system@zlg.cn'
+    'address': 'XXXX',
+    'username': 'XXXX',
+    'password': 'XXXXXX',
+    'sender': 'XXXX'
 }
 ac_user_info = {
     'user': 'admin',
-    'password': 'zlgmcu!@#123'
+    'password': 'XXXX'
 
 }
